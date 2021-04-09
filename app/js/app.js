9244bbea4bb168757b77dc6868797ba67cf44ddf
CJEF <ef2200@mail.ru>
2021-04-09 14:37:24 +0600

    js

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,9 @@
-window.onload(function() {
+// window.onload(function() {
 
-	let preloader = $(".preloader");
-  	let gif = $(".lazy-mackbook");
+// 	let preloader = $(".preloader");
+//   	let gif = $(".lazy-mackbook");
 
-	gif.hide();
-	preloader.addClass('ready')
+// 	gif.hide();
+// 	preloader.addClass('ready')
 
-})
+// })
